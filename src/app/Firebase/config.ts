a7d41149eaaa6fce911fe8008d9bbc8db96a23b5
Yashas Bysani Muralidhar <yashasbysani@gmail.com>
2019-06-18 19:34:54 -0400

    Update config.ts

--- src/app/Firebase/config.ts
+++ src/app/Firebase/config.ts
@@ -1,11 +1,11 @@
 const config = {
-    apiKey: "AIzaSyBIRMoA10F-ytFt9jYu9T6GZOk7wnRNEyY",
-    authDomain: "clone-2c41a.firebaseapp.com",
-    databaseURL: "https://clone-2c41a.firebaseio.com",
-    projectId: "clone-2c41a",
-    storageBucket: "clone-2c41a.appspot.com",
-    messagingSenderId: "462873028835",
-    appId: "1:462873028835:web:a94f58e1473bba35"
+    apiKey: "randomKey",
+    authDomain: "clone.firebaseapp.com",
+    databaseURL: "https://clone.firebaseio.com",
+    projectId: "clone",
+    storageBucket: "clone.appspot.com",
+    messagingSenderId: "46287302",
+    appId: "1:46287302:web:a94f58e1473bba35"
 }
 
 export default config;
